ca6420618e7bca3415eb991248a75e422220f867
Patrick Carton <pcarton927@gmail.com>
2017-05-26 14:48:18 -0400

    added simple test script

---
+++ test-hammer.sh
@@ -0,0 +1,9 @@
+echo "PCarton test message with no ban content" | python3 hammer.py
+
+echo "PCarton test message with ban content : blacklist" | python3 hammer.py
+
+echo "PCarton test message with ban content : meany" | python3 hammer.py
+
+echo "PCarton test message with ban content : testWord" | python3 hammer.py
+
+echo "PCarton test message with ban content : Testword" | python3 hammer.py
