854b8d283effb55af291eb5b982c7a98b6aad903
Patrick Carton <pcarton927@gmail.com>
2017-05-31 15:42:29 -0400

    initial commit

---
+++ ganon.py
@@ -0,0 +1,4 @@
+#Link handling module - used for permits and such
+# Returns info to the driver as a string in the form 'action user responseMsg'
+# Expects info in form 'username highestRole messagetext'
+import sys
