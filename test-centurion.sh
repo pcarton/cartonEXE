10be3122dbd66d68071e9898fd36c884cb53accc
Patrick Carton <pcarton927@gmail.com>
2017-06-04 14:33:28 -0400

    updated test script

--- test-centurion.sh
+++ test-centurion.sh
@@ -1,2 +1,11 @@
 echo "PCarton Mod !add !test Mod Hello World"
 echo "PCarton Mod !add !test Mod Hello World" | python3 centurion.py
+
+echo "PCarton Mod !test"
+echo "PCarton Mod !test" | python3 centurion.py
+
+echo "PCarton Mod !remove !test"
+echo "PCarton Mod !remove !test" | python3 centurion.py
+
+echo "PCarton Mod !test"
+echo "PCarton Mod !test" | python3 centurion.py
