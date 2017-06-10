a7039cf8cf6626513bacddcc8f4d2b55e4f61b37
Patrick Carton <pcarton927@gmail.com>
2017-06-10 17:27:28 -0400

    added initial tests for purge,timeout,ban,unban commands

--- test-centurion.sh
+++ test-centurion.sh
@@ -1,3 +1,5 @@
+
+
 echo "PCarton Mod !add !test Mod Hello World"
 echo "PCarton Mod !add !test Mod Hello World" | python3 centurion.py
 
@@ -19,3 +21,27 @@ echo "PCarton Mod !add !testingInvalidArgs Hello World" | python3 centurion.py
 
 echo "PCarton Mod !add"
 echo "PCarton Mod !add" | python3 centurion.py
+
+echo "PCarton Mod !purge PCarton"
+echo "PCarton Mod !purge PCarton" | python3 centurion.py
+
+echo "PCarton Mod !purge"
+echo "PCarton Mod !purge" | python3 centurion.py
+
+echo "PCarton Mod !timeout PCarton"
+echo "PCarton Mod !timeout PCarton" | python3 centurion.py
+
+echo "PCarton Mod !timeout"ban
+echo "PCarton Mod !timeout" | python3 centurion.py
+
+echo "PCarton Mod !ban PCarton"
+echo "PCarton Mod !ban PCarton" | python3 centurion.py
+
+echo "PCarton Mod !ban"
+echo "PCarton Mod !ban" | python3 centurion.py
+
+echo "PCarton Mod !unban PCarton"
+echo "PCarton Mod !unban PCarton" | python3 centurion.py
+
+echo "PCarton Mod !unban"
+echo "PCarton Mod !unban" | pytimeoutthon3 centurion.py
