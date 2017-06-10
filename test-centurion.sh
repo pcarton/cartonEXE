e4363f806c0e487f1e3cbd969370b934a2b51c79
Patrick Carton <pcarton927@gmail.com>
2017-06-10 17:34:44 -0400

    added color to test inputs

--- test-centurion.sh
+++ test-centurion.sh
@@ -5,50 +5,50 @@ LIGHTBLUE='\033[1;34m'
 GREEN='\033[1;32m'
 NC='\033[0m'
 
-echo -e $GREEN Testing !add, !remove, and database commands $NC
-echo "PCarton Mod !add !test Mod Hello World"
+echo -e "$GREEN Testing !add, !remove, and database commands $NC"
+echo -e "$LIGHTBLUE PCarton Mod !add !test Mod Hello World"
 echo "PCarton Mod !add !test Mod Hello World" | python3 centurion.py
 
-echo "PCarton Mod !test"
+echo -e "$LIGHTBLUE PCarton Mod !test $NC"
 echo "PCarton Mod !test" | python3 centurion.py
 
-echo "PCarton Mod !remove !test"
+echo -e "$LIGHTBLUE PCarton Mod !remove !test $NC"
 echo "PCarton Mod !remove !test" | python3 centurion.py
 
-echo "PCarton Mod !test"
+echo -e "$LIGHTBLUE PCarton Mod !test $NC"
 echo "PCarton Mod !test" | python3 centurion.py
 
 
-echo "PCarton Mod !add !testingInvalidArgs Mod"
+echo -e "$LIGHTBLUE PCarton Mod !add !testingInvalidArgs Mod $NC"
 echo "PCarton Mod !add !testingInvalidArgs Mod" | python3 centurion.py
 
-echo "PCarton Mod !add !testingInvalidArgs Hello World"
+echo -e "$LIGHTBLUE PCarton Mod !add !testingInvalidArgs Hello World $NC"
 echo "PCarton Mod !add !testingInvalidArgs Hello World" | python3 centurion.py
 
-echo "PCarton Mod !add"
+echo -e "$LIGHTBLUE PCarton Mod !add $NC"
 echo "PCarton Mod !add" | python3 centurion.py
 
-echo -e $GREEN Testing !purge, !timeout, !ban, !unban commands $NC
-echo "PCarton Mod !purge PCarton"
+echo -e "$GREEN Testing !purge, !timeout, !ban, !unban commands $NC"
+echo " $LIGHTBLUE PCarton Mod !purge PCarton $NC"
 echo "PCarton Mod !purge PCarton" | python3 centurion.py
 
-echo "PCarton Mod !purge"
+echo -e "$LIGHTBLUE PCarton Mod !purge $NC"
 echo "PCarton Mod !purge" | python3 centurion.py
 
-echo "PCarton Mod !timeout PCarton"
+echo -e "$LIGHTBLUE PCarton Mod !timeout PCarton $NC"
 echo "PCarton Mod !timeout PCarton" | python3 centurion.py
 
-echo "PCarton Mod !timeout"ban
+echo -e "$LIGHTBLUE PCarton Mod !timeout $NC"
 echo "PCarton Mod !timeout" | python3 centurion.py
 
-echo "PCarton Mod !ban PCarton"
+echo -e "$LIGHTBLUE PCarton Mod !ban PCarton $NC"
 echo "PCarton Mod !ban PCarton" | python3 centurion.py
 
-echo "PCarton Mod !ban"
+echo -e "$LIGHTBLUE PCarton Mod !ban $NC"
 echo "PCarton Mod !ban" | python3 centurion.py
 
-echo "PCarton Mod !unban PCarton"
+echo -e "$LIGHTBLUE PCarton Mod !unban PCarton $NC"
 echo "PCarton Mod !unban PCarton" | python3 centurion.py
 
-echo "PCarton Mod !unban"
+echo -e "$LIGHTBLUE PCarton Mod !unban $NC"
 echo "PCarton Mod !unban" | python3 centurion.py
