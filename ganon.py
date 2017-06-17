1a2c4c971ab618681ba8319b7b31a21cd775698b
Patrick Carton <pcarton927@gmail.com>
2017-06-17 15:45:50 -0400

    added headers of needed functions and load config function

--- ganon.py
+++ ganon.py
@@ -1,4 +1,32 @@
 #Link handling module - used for permits and such
 # Returns info to the driver as a string in the form 'action user responseMsg'
 # Expects info in form 'username highestRole messagetext'
-import sys
+import sys,pymysql
+
+conn = None
+dbAddr = ""
+dbUser = ""
+dbPass = ""
+db = ""
+debug = False
+
+#Get the config data
+def loadConfig():
+    global dbAddr, dbPass, dbUser, db, debug
+    with open('config.json') as data:
+        config = json.load(data)
+        debug = config["debug"]
+        dbAddr = config["databaseURL"]
+        dbPass = config["databasePassword"]
+        dbUser = config["databaseUser"]
+        db = config["databaseName"]
+        data.close()
+
+def removePermit(username):
+
+def addPermit(username):
+
+def getPermit(username):
+
+def isPermitted(username):
+    return not getPermit(username) == None
