f3724107f99592f6df8dc6c86da5471a00ccc3fa
Patrick Carton <pcarton927@gmail.com>
2017-06-18 17:10:31 -0400

    implemented addPermit

--- ganon.py
+++ ganon.py
@@ -26,7 +26,7 @@ def loadConfig():
 #returns True if action success, False if exception encountered
 def removePermit(username):
     #Load globals needed
-    global conn
+    global conn, debug
     #make sure we have a connection to DB
     if conn == None:
         connect()
@@ -46,11 +46,40 @@ def removePermit(username):
         return False
 
 def addPermit(username): #TODO
+    #Load globals needed
+    global conn, debug
+
+    #make sure we have a connection to DB
+    if conn == None:
+        connect()
+
+    #Make the SQL statement
+    update = "INSERT INTO permits(user,expiration) VALUES('{0}','{1}')"
+
+    #make the expiration object
+    time = datetime.datetime.utcnow() + datetime.timedelta(minutes=10)
+    timeStr = pythonToString(time)
+    if debug:
+        print(time)
+        print(timeStr)
+
+    #Create cursor to execute query
+    cursor = conn.cursor()
+    #try the insert, rollback if error
+    try:
+        cursor.execute(update.format(username, timeStr))
+        conn.commit()
+        return True
+    except Exception as e:
+        if debug:
+            print(e)
+        conn.rollback()
+        return False
 
 #returns the expiration date of the user's permit, or None if no permit exists
 def getPermit(username):
     #Load globals needed
-    global conn
+    global conn, debug
 
     #prepare default return values
     expir = None
