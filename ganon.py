14d67e02486610cd15b1e0fed692b45d29e90566
Patrick Carton <pdc21446@uga.edu>
2017-06-19 02:23:34 -0400

    added missing import

--- ganon.py
+++ ganon.py
@@ -1,7 +1,7 @@
 #Link handling module - used for permits and such
 # Returns info to the driver as a string in the form 'action user responseMsg'
 # Expects info in form 'username highestRole messagetext'
-import sys, pymysql, datetime
+import sys, pymysql, datetime, json
 
 conn = None
 dbAddr = ""
