8ca2144b278e263651abfda5c8462e71f80cdd6f
Patrick Carton <pcarton927@gmail.com>
2017-06-20 18:55:55 -0400

    started test-ganon script

---
+++ test-ganon.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#colors
+LIGHTBLUE='\033[1;34m'
+GREEN='\033[1;32m'
+NC='\033[0m'
+
+echo -e "${LIGHTBLUE}PCarton Mod google.com $NC"
+echo "PCarton Mod google.com" | python3 ganon.py
