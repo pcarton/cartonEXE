fc190a3971102fbcc23bd207d020fbf8c1443ccb
Patrick Carton <pcarton927@gmail.com>
2017-06-29 18:16:51 -0400

    started script to clear test data from MySQL db

---
+++ remove-test-data.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#colors
+LIGHTBLUE='\033[1;34m'
+GREEN='\033[1;32m'
+NC='\033[0m'
+
+echo -e "${LIGHTBLUE}PCarton Mod !remove !test $NC"
+echo "PCarton Mod !remove !test" | python3 centurion.py
+
+echo -e "${LIGHTBLUE}PCarton Mod !remove !test $NC"
+echo "PCarton Mod !remove !test2" | python3 centurion.py
