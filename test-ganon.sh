058aedf4e28a6464f8a82b6e34a467fcc6a3dd7d
Patrick Carton <pcarton927@gmail.com>
2017-07-01 16:48:04 -0400

    updated test file to match input format of laser.js

--- test-ganon.sh
+++ test-ganon.sh
@@ -5,5 +5,17 @@ LIGHTBLUE='\033[1;34m'
 GREEN='\033[1;32m'
 NC='\033[0m'
 
-echo -e "${LIGHTBLUE}PCarton Mod google.com $NC"
-echo "PCarton Mod google.com" | python3 ganon.py
+echo -e "${LIGHTBLUE}PCarton Normal $NC"
+echo "PCarton Normal" | python3 ganon.py
+
+echo -e "${LIGHTBLUE}PCarton Follower $NC"
+echo "PCarton Follower" | python3 ganon.py
+
+echo -e "${LIGHTBLUE}PCarton Sub $NC"
+echo "PCarton Sub" | python3 ganon.py
+
+echo -e "${LIGHTBLUE}PCarton Mod $NC"
+echo "PCarton Mod" | python3 ganon.py
+
+echo -e "${LIGHTBLUE}PCarton Caster $NC"
+echo "PCarton Caster" | python3 ganon.py
