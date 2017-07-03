1eafa2a586b1429feef1278a4db726d633d5d405
Patrick Carton <pcarton927@gmail.com>
2017-07-03 19:44:15 -0400

    whitespace fix

--- test-centurion.py
+++ test-centurion.py
@@ -34,127 +34,127 @@ class centurionTest(unittest.TestCase):
     def testHasAccessCasterCaster(self):
         roleHad = "Caster"
         roleNeeded = "Caster"
-       self.assertEqual(centurion.hasAccess(roleHad,roleNeeded),True)
+        self.assertEqual(centurion.hasAccess(roleHad,roleNeeded),True)
 
     def testHasAccessCasterMod(self):
         roleHad = "Caster"
         roleNeeded = "Mod"
-       self.assertEqual(centurion.hasAccess(roleHad,roleNeeded),True)
+        self.assertEqual(centurion.hasAccess(roleHad,roleNeeded),True)
 
     def testHasAccessCasterSub(self):
         roleHad = "Caster"
         roleNeeded = "Sub"
-       self.assertEqual(centurion.hasAccess(roleHad,roleNeeded),True)
+        self.assertEqual(centurion.hasAccess(roleHad,roleNeeded),True)
 
     def testHasAccessCasterFollower(self):
         roleHad = "Caster"
         roleNeeded = "Follower"
-       self.assertEqual(centurion.hasAccess(roleHad,roleNeeded),True)
+        self.assertEqual(centurion.hasAccess(roleHad,roleNeeded),True)
 
     def testHasAccessCasterNormal(self):
         roleHad = "Caster"
         roleNeeded = "Normal"
-       self.assertEqual(centurion.hasAccess(roleHad,roleNeeded),True)
+        self.assertEqual(centurion.hasAccess(roleHad,roleNeeded),True)
 
     def testHasAccessModCaster(self):
         roleHad = "Mod"
         roleNeeded = "Caster"
-       self.assertEqual(centurion.hasAccess(roleHad,roleNeeded),False)
+        self.assertEqual(centurion.hasAccess(roleHad,roleNeeded),False)
 
     def testHasAccessModMod(self):
         roleHad = "Mod"
         roleNeeded = "Mod"
-       self.assertEqual(centurion.hasAccess(roleHad,roleNeeded),True)
+        self.assertEqual(centurion.hasAccess(roleHad,roleNeeded),True)
 
     def testHasAccessModSub(self):
         roleHad = "Mod"
         roleNeeded = "Sub"
-       self.assertEqual(centurion.hasAccess(roleHad,roleNeeded),True)
+        self.assertEqual(centurion.hasAccess(roleHad,roleNeeded),True)
 
     def testHasAccessModFollower(self):
         roleHad = "Mod"
         roleNeeded = "Follower"
-       self.assertEqual(centurion.hasAccess(roleHad,roleNeeded),True)
+        self.assertEqual(centurion.hasAccess(roleHad,roleNeeded),True)
 
     def testHasAccessModNormal(self):
         roleHad = "Mod"
         roleNeeded = "Normal"
-       self.assertEqual(centurion.hasAccess(roleHad,roleNeeded),True)
+        self.assertEqual(centurion.hasAccess(roleHad,roleNeeded),True)
 
     def testHasAccessSubCaster(self):
         roleHad = "Sub"
         roleNeeded = "Caster"
-       self.assertEqual(centurion.hasAccess(roleHad,roleNeeded),False)
+        self.assertEqual(centurion.hasAccess(roleHad,roleNeeded),False)
 
     def testHasAccessSubMod(self):
         roleHad = "Sub"
         roleNeeded = "Mod"
-       self.assertEqual(centurion.hasAccess(roleHad,roleNeeded),False)
+        self.assertEqual(centurion.hasAccess(roleHad,roleNeeded),False)
 
     def testHasAccessSubSub(self):
         roleHad = "Sub"
         roleNeeded = "Sub"
-       self.assertEqual(centurion.hasAccess(roleHad,roleNeeded),True)
+        self.assertEqual(centurion.hasAccess(roleHad,roleNeeded),True)
 
     def testHasAccessSubFollower(self):
         roleHad = "Sub"
         roleNeeded = "Follower"
-       self.assertEqual(centurion.hasAccess(roleHad,roleNeeded),True)
+        self.assertEqual(centurion.hasAccess(roleHad,roleNeeded),True)
 
     def testHasAccessSubNormal(self):
         roleHad = "Sub"
         roleNeeded = "Normal"
-       self.assertEqual(centurion.hasAccess(roleHad,roleNeeded),True)
+        self.assertEqual(centurion.hasAccess(roleHad,roleNeeded),True)
 
     def testHasAccessFollowerCaster(self):
         roleHad = "Follower"
         roleNeeded = "Caster"
-       self.assertEqual(centurion.hasAccess(roleHad,roleNeeded),False)
+        self.assertEqual(centurion.hasAccess(roleHad,roleNeeded),False)
 
     def testHasAccessFollowerMod(self):
         roleHad = "Follower"
         roleNeeded = "Mod"
-       self.assertEqual(centurion.hasAccess(roleHad,roleNeeded),False)
+        self.assertEqual(centurion.hasAccess(roleHad,roleNeeded),False)
 
     def testHasAccessFollowerSub(self):
         roleHad = "Follower"
         roleNeeded = "Sub"
-       self.assertEqual(centurion.hasAccess(roleHad,roleNeeded),False)
+        self.assertEqual(centurion.hasAccess(roleHad,roleNeeded),False)
 
     def testHasAccessFollowerFollower(self):
         roleHad = "Follower"
         roleNeeded = "Follower"
-       self.assertEqual(centurion.hasAccess(roleHad,roleNeeded),True)
+        self.assertEqual(centurion.hasAccess(roleHad,roleNeeded),True)
 
     def testHasAccessFollowerNormal(self):
         roleHad = "Follower"
         roleNeeded = "Normal"
-       self.assertEqual(centurion.hasAccess(roleHad,roleNeeded),True)
+        self.assertEqual(centurion.hasAccess(roleHad,roleNeeded),True)
 
     def testHasAccessNormalCaster(self):
         roleHad = "Caster"
         roleNeeded = "Caster"
-       self.assertEqual(centurion.hasAccess(roleHad,roleNeeded),False)
+        self.assertEqual(centurion.hasAccess(roleHad,roleNeeded),False)
 
     def testHasAccessNormalMod(self):
         roleHad = "Caster"
         roleNeeded = "Mod"
-       self.assertEqual(centurion.hasAccess(roleHad,roleNeeded),False)
+        self.assertEqual(centurion.hasAccess(roleHad,roleNeeded),False)
 
     def testHasAccessNormalSub(self):
         roleHad = "Caster"
         roleNeeded = "Sub"
-       self.assertEqual(centurion.hasAccess(roleHad,roleNeeded),False)
+        self.assertEqual(centurion.hasAccess(roleHad,roleNeeded),False)
 
     def testHasAccessNormalFollower(self):
         roleHad = "Caster"
         roleNeeded = "Follower"
-       self.assertEqual(centurion.hasAccess(roleHad,roleNeeded),False)
+        self.assertEqual(centurion.hasAccess(roleHad,roleNeeded),False)
 
     def testHasAccessNormalNormal(self):
         roleHad = "Caster"
         roleNeeded = "Normal"
-       self.assertEqual(centurion.hasAccess(roleHad,roleNeeded),True)
+        self.assertEqual(centurion.hasAccess(roleHad,roleNeeded),True)
 
 if __name__ == "__main__":
             unittest.main()
