29760a09b2d834df21659be413607c19572ac543
Patrick Carton <pcarton927@gmail.com>
2017-07-03 20:13:57 -0400

    added clearing color to first call

--- testCartonBot.sh
+++ testCartonBot.sh
@@ -5,7 +5,7 @@ LIGHTBLUE='\033[1;34m'
 GREEN='\033[1;32m'
 NC='\033[0m'
 
-echo -e "${GREEN}Reseting Test Database"
+echo -e "${GREEN}Reseting Test Database $NC"
 mysql -uroot -proot cartonBotTest < schema-test.sql
 
 echo -e "${GREEN}Testing Centurion $NC"
