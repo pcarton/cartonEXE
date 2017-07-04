71decd7c89b85eb387f2f406abc4fbe796042120
Patrick Carton <pcarton927@gmail.com>
2017-07-03 20:10:08 -0400

    started master test script

---
+++ testCartonBot.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#colors
+LIGHTBLUE='\033[1;34m'
+GREEN='\033[1;32m'
+NC='\033[0m'
+
+echo -e "${GREEN}Reseting Test Database"
+mysql -uroot -proot cartonBotTest < schema-test.sql
+
+echo -e "${GREEN}Testing Centurion $NC"
+python3 test-centurion.py
