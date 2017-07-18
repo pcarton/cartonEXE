8f1e6c94b526e92bb1c16761bc1852f5c907d7bd
Patrick Carton <pcarton927@gmail.com>
2017-07-18 13:21:42 -0400

    added initial test file for ganon module

---
+++ test-ganon.py
@@ -0,0 +1,36 @@
+import unittest
+from subprocess import call
+
+import ganon
+
+class ganonTest(unittest.TestCase):
+    def setUp(self):
+        self.CONFIG_PATH = 'config-test.json'
+
+#TESTS FOR IF THE CONFIG LOADED CORRECTLY
+    def testLoadConfigAddr(self):
+        ganon.loadConfig(self.CONFIG_PATH)
+        self.assertEqual(ganon.dbAddr, "localhost")
+
+    def testLoadConfigPass(self):
+        ganon.loadConfig(self.CONFIG_PATH)
+        self.assertEqual(ganon.dbPass, "root")
+
+    def testLoadConfigUser(self):
+        ganon.loadConfig(self.CONFIG_PATH)
+        self.assertEqual(ganon.dbUser, "root")
+
+    def testLoadConfigName(self):
+        ganon.loadConfig(self.CONFIG_PATH)
+        self.assertEqual(ganon.db, "cartonBotTest")
+
+    def testLoadConfigDebug(self):
+        ganon.loadConfig(self.CONFIG_PATH)
+        self.assertEqual(ganon.debug, False)
+
+    def testLoadConfigWhitelist(self):
+        ganon.loadConfig(self.CONFIG_PATH)
+        self.assertEqual(ganon.whitelisted, ["StreamJar","TestUser2"])
+
+if __name__ == "__main__":
+        unittest.main()
