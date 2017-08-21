1a9e2ab363c7973528d69ef06d97447328fd16b0
Patrick Carton <pcarton927@gmail.com>
2017-08-21 10:46:35 -0400

    added initial scribe and test-scibe files for the logging module

---
+++ test-scribe.py
@@ -0,0 +1,24 @@
+import unittest
+from subprocess import call
+import datetime
+
+class scibeTest(unittest.TestCase):
+    def setUp(self):
+        self.CONFIG_PATH = 'config-test.json'
+
+#TESTS FOR IF THE CONFIG LOADED CORRECTLY
+    def testLoadConfigLogFile(self):
+        scribe.loadConfig(self.CONFIG_PATH)
+        self.assertEqual(scibe.logFile, "testLog")
+
+    def testLoadConfigLogFile(self):
+        scribe.loadConfig(self.CONFIG_PATH)
+        self.assertEqual(scibe.logExtension, "txt")
+
+    def testLoadConfigDebug(self):
+        scribe.loadConfig(self.CONFIG_PATH)
+        self.assertEqual(scibe.debug, False)
+
+    #TODO rest of tests
+if __name__ == "__main__":
+        unittest.main()
