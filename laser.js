e1120a3b702409e47667a42fe7509a4fbe653bfc
Patrick Carton <pcarton927@gmail.com>
2017-09-16 15:30:11 -0400

    reverted changes and fixed typo

--- laser.js
+++ laser.js
@@ -26,7 +26,7 @@ client.use('oauth',{
 });
 
 //get following channel and join its chat
-client.request('GET', 'channels/current')
+client.request('GET', 'users/current')
 .then(response => {
     if(debug) console.log(response.body);
     userInfo = response.body;
@@ -36,14 +36,14 @@ client.request('GET', 'channels/current')
 client.request('GET', 'channels/'+config.channelUsername)
 .then(response =>{
   if(debug){
-    return client.chat.join(userInfo.user.channel.id);
+    return client.chat.join(userInfo.channel.id);
   }else{
     if(response.body[0]){
       channelId = response.body.id;
       console.log("Joining",response.body.name);
       return client.chat.join(channelId);
     }else{
-      return client.chat.join(userInfo.user.channel.id);
+      return client.chat.join(userInfo.channel.id);
     }
   }
 })
