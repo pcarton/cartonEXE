d9c83e09a9ddc35840d3b6254a8e4369d087d177
Patrick Carton <pcarton927@gmail.com>
2017-10-14 11:42:45 -0400

    fixed file paths

--- tests/testCartonBot.sh
+++ tests/testCartonBot.sh
@@ -9,22 +9,22 @@ echo -e "${GREEN}Reseting Test Database $NC"
 mysql -uroot -proot cartonBotTest < ./schema-test.sql
 
 echo -e "${GREEN}Testing Centurion $NC"
-python3 ../py-modules/py-tests/test-centurion.py
+python3 ../PyMods/Tests/test-centurion.py
 
 echo -e "${GREEN}Testing Centurion Add/Remove $NC"
-python3 ../py-modules/py-tests/test-centurion-add-remove.py
+python3 ../PyMods/Tests/test-centurion-add-remove.py
 
 echo -e "${GREEN}Reseting Test Database $NC"
 mysql -uroot -proot cartonBotTest < ./schema-test.sql
 
 echo -e "${GREEN}Testing Ganon $NC"
-python3 ../py-modules/py-tests/test-ganon.py
+python3 ../PyMods/Tests/test-ganon.py
 
 echo -e "${GREEN}Reseting Test Database $NC"
 mysql -uroot -proot cartonBotTest < ./schema-test.sql
 
 echo -e "${GREEN}Testing Hammer $NC"
-python3 ../py-modules/py-tests/test-hammer.py
+python3 ../PyMods/Tests/test-hammer.py
 
 echo -e "${GREEN}Testing Scribe $NC"
-python3 ../py-modules/py-tests/test-scribe.py
+python3 ../PyMods/Tests/test-scribe.py
