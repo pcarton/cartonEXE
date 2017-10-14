3e02571e95d346f2a1a196eaf007567055fa48a7
Patrick Carton <pcarton927@gmail.com>
2017-10-14 12:22:23 -0400

    updated test script

--- tests/testCartonBot.sh
+++ tests/testCartonBot.sh
@@ -5,6 +5,14 @@ LIGHTBLUE='\033[1;34m'
 GREEN='\033[1;32m'
 NC='\033[0m'
 
+#PythonPath STUFF
+CURRENT_DIR = pwd
+PARENT_DIR = ${CURRENT_DIR:0:${#CURRENT_DIR}-6}
+echo PARENT_DIR
+OLD_PYTHONPATH = ${PYTHONPATH}
+PYTHONPATH = "${OLD_PYTHONPATH}:${PARENT_DIR}"
+
+
 echo -e "${GREEN}Reseting Test Database $NC"
 mysql -uroot -proot cartonBotTest < ./schema-test.sql
 
@@ -28,3 +36,5 @@ python3 ../PyMods/Tests/test-hammer.py
 
 echo -e "${GREEN}Testing Scribe $NC"
 python3 ../PyMods/Tests/test-scribe.py
+
+PYTHONPATH = ${OLD_PYTHONPATH}
