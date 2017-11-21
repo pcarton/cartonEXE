175716b01972d7c4381ea60f05aec023c07970cc
Patrick Carton <pcarton927@gmail.com>
2017-11-21 14:14:52 -0500

    added debug command

--- chaos.js
+++ chaos.js
@@ -1,6 +1,8 @@
 var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
+const serverID = auth.serverID;
+
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, {
@@ -22,18 +24,35 @@ bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
     if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];       
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-            break;
-            // Just add any case commands if you want to..
-         }
-     }
+      parseCommand(user, userID, channelID, message, evt);
+    }
 });
+
+function parseCommand(user, userID, channelID, message, evt){
+  var args = message.substring(1).split(' ');
+  var cmd = args[0];
+  args = args.splice(1);
+  switch(cmd) {
+      // !ping
+      case 'ping':
+          bot.sendMessage({
+              to: channelID,
+              message: 'Pong!'
+          });
+      break;
+      case 'getServerID':
+          bot.sendMessage({
+              to: channelID,
+              message: "CHANNELID:" + channelID + ", USERID:" + userID,
+          });
+      break;
+      case 'warframe':
+
+        bot.addToRole({
+          "serverID": serverID,
+          "userID": userID,
+          "roleID": "Warframe"
+        });
+      break;
+   }
+}
