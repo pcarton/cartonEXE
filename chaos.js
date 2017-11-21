d31be4476704805668b13d032f1656bd291a3068
Patrick Carton <pcarton927@gmail.com>
2017-11-21 14:51:45 -0500

    added a role command

--- chaos.js
+++ chaos.js
@@ -13,6 +13,19 @@ client.on('message', message => {
   if (message.content === 'ping') {
     message.reply('pong');
   }
+  if(message.content === '!warframe'){
+    var user = message.member;
+    var server = message.guild;
+    var roles = server.roles;
+    for(var role in roles){
+      if(role.name === "Warframe"){
+        user.addRole(role);
+        message.reply('Added you to the Warframe Group');
+        break;
+      }
+    }
+    message.reply('Could not add you to the Warframe Group due to an error');
+  }
 });
 
 client.login(auth.token);
