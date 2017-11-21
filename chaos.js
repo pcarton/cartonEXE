9f7d8ac1e0ef7bdb0ad24d51437da8bebad2e911
Patrick Carton <pcarton927@gmail.com>
2017-11-21 16:37:10 -0500

    logic fixes

--- chaos.js
+++ chaos.js
@@ -24,16 +24,17 @@ function addToRole(message, roleName){
   var currRoles = user.roles;
   console.log("User:"+user+" Server:"+server);
   console.log(roles.size);
-  roles.every(function(role){
-    console.log(role.name);
-    if(currRoles.find('name',role.name) !== undefined){
-      message.reply('You are already in the ' + roleName + ' Group');
-    }else if(role.name === roleName){
-      user.addRole(role);
-      message.reply('Added you to the ' + roleName + ' Group');
-      console.log('ADDED USER '+ user.name + ' TO ROLE '+ roleName);
-    }
-  });
+  roles.every(
+    function(role){
+      console.log(role.name);
+      if(currRoles.find('name',roleName) !== undefined){
+        message.reply('You are already in the ' + roleName + ' Group');
+      }else if(role.name === roleName){
+        user.addRole(role);
+        message.reply('Added you to the ' + roleName + ' Group');
+        console.log('ADDED USER '+ user.name + ' TO ROLE '+ roleName);
+      }
+    });
   message.reply('Could not add you to the ' + roleName + ' Group due to an error');
   console.log('[!]ERROR ADDING USER '+ user.name + ' TO ROLE '+ roleName);
 }
