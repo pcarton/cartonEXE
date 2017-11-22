d2775636c101e349afb2813c2f4a1407aef2586e
Patrick Carton <pcarton927@gmail.com>
2017-11-21 20:31:36 -0500

    added channel check

--- chaos.js
+++ chaos.js
@@ -10,14 +10,16 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-  if(message.content === '!warframe'){
-    addToRole(message,"Warframe");
-  }else if(message.content === '!eso'){
-    addToRole(message,"ESO");
-  }else if(message.content === '!diablo'){
-    addToRole(message,"Diablo");
-  }else if(message.content === '!fortnite'){
-    addToRole(message,"Fortnite");
+  if(message.channel.name === "bot"){
+    if(message.content === '!warframe'){
+      addToRole(message,"Warframe");
+    }else if(message.content === '!eso'){
+      addToRole(message,"ESO");
+    }else if(message.content === '!diablo'){
+      addToRole(message,"Diablo");
+    }else if(message.content === '!fortnite'){
+      addToRole(message,"Fortnite");
+    }
   }
 });
 
